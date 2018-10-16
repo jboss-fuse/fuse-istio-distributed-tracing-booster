50926b9f3fa540743ed4fd0d34c5159be13b72f2
Andrea Tarocchi <atarocch@redhat.com>
2018-10-16 13:41:15 +0200

    Fixed openshift dependecies in itest

--- itest/src/test/java/com/redhat/fuse/boosters/istio/dt/IstioDistributedTracingBoosterKT.java
+++ itest/src/test/java/com/redhat/fuse/boosters/istio/dt/IstioDistributedTracingBoosterKT.java
@@ -1,7 +1,8 @@
 package com.redhat.fuse.boosters.istio.dt;
 
-import io.fabric8.kubernetes.api.model.v2_6.HasMetadata;
-import io.fabric8.openshift.clnt.v2_6.OpenShiftClient;
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.openshift.client.OpenShiftClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Test;
